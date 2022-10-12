3a4e018876ab89141b718119578bd95827e3859e
Fernando Trevino <fernando.yanez.trevino@gmail.com>
2022-10-13 00:21:16 +0200

    Update code with deps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { Amplify, API } from 'aws-amplify';
+import awsconfig from './aws-exports';
 
+Amplify.configure(awsconfig);
 function App() {
   return (
     <div className="App">
