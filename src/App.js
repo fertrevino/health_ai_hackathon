60a9268900f02030f3516fa9fbb94773ac5ce906
Fernando Trevino <fernando.yanez.trevino@gmail.com>
2022-10-13 00:33:54 +0200

    Add get request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import { Amplify, API } from 'aws-amplify';
-import awsconfig from './aws-exports';
+import logo from "./logo.svg";
+import "./App.css";
+import { Amplify, API } from "aws-amplify";
+import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
+
 function App() {
+  // GET requests
+  const apiName = "comprehendMedical";
+  const path = "/description";
+  const myInit = {
+    headers: {}, // OPTIONAL
+    response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+    // queryStringParameters: {
+    //   name: "param", // OPTIONAL
+    // },
+  };
+
+  API.get(apiName, path, myInit)
+    .then((response) => {
+      console.log(`Response from GET: ${JSON.stringify(response)}`);
+    })
+    .catch((error) => {
+      console.log(`Error from response: ${JSON.stringify(error.response)}`);
+    });
+
   return (
     <div className="App">
       <header className="App-header">
